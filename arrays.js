167b42e9332f2285f0f00f334e3dd5176701885c
gorgeous-monitor-5108 <ryanshuken@gmail.com>
2020-07-14 00:33:14 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -17,20 +17,15 @@ function destructivelyAddElementToBeginningOfArray(array,element) {
 
 function addElementToEndOfArray(array,element) 
   {
-  return []
-  }
-
-
-/*
-function addElementToEndOfArray(array,element) 
-  {
-  
+  return [...array,element];
   }
 
 function destructivelyAddElementToEndOfArray(array,element) {
-  
+  array.push(element);
+  return array;
   }
-
+  
+/*
 function destructivelyAddElementToEndOfArray(array,element) {
   
   }
