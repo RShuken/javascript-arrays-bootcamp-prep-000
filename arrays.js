ea386bc0d5276be3620bc1ecd49a35d8bafd4363
gorgeous-monitor-5108 <ryanshuken@gmail.com>
2020-07-14 00:34:58 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -10,17 +10,17 @@ function addElementToBeginningOfArray(array,element) {
   return [element,...array];
   }
 
-function destructivelyAddElementToBeginningOfArray(array,element) {
+function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
   return array;
   }
 
-function addElementToEndOfArray(array,element) 
+function addElementToEndOfArray(array, element) 
   {
   return [...array,element];
   }
 
-function destructivelyAddElementToEndOfArray(array,element) {
+function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element);
   return array;
   }
