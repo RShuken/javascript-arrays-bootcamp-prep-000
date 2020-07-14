74e0d000b2d21fd7a0e98c430c234119a3032cde
gorgeous-monitor-5108 <ryanshuken@gmail.com>
2020-07-14 00:22:51 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -8,9 +8,36 @@ var chocolateBars = [
 
 function addElementToBeginningOfArray(array,element) {
   return [element,...array];
-}
+  }
 
 function destructivelyAddElementToBeginningOfArray(array,element) {
   array.unshift(element);
   return array;
-}
+  }
+
+function addElementToEndOfArray(array, element) 
+  {
+  
+  }
+
+function addElementToEndOfArray(array, element) 
+  {
+  
+  }
+
+function destructivelyAddElementToEndOfArray(array,element) {
+  
+  }
+
+function destructivelyAddElementToEndOfArray(array,element) {
+  
+  }
+  
+function accessElementInArray(array,element) 
+  {
+  
+  }
+  
+function destructivelyRemoveElementFromBeggingOfArray(array) {
+  
+  }
