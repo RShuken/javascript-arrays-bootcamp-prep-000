09fc9a8ce393b9fd3cb77e0cf791141e3f36e2a0
gorgeous-monitor-5108 <ryanshuken@gmail.com>
2020-07-14 00:17:45 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -11,6 +11,6 @@ function addElementToBeginningOfArray(array,element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array,element) {
-  array = [element,...array];
-  return array;
+  array.unshift(element);
+return array;
 }
