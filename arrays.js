9f3dd89679d5e51c9ef47a716d28401bae5841c6
gorgeous-monitor-5108 <ryanshuken@gmail.com>
2020-07-14 00:13:58 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -7,9 +7,11 @@ var chocolateBars = [
 ];
 
 function addElementToBeginningOfArray(array,element) {
-  
+  const newArrary = [element,...array];
+  return newArrary;
 }
 
 function destructivelyAddElementToBeginningOfArray(array,element) {
-  
+  array = [element,...array];
+  return array;
 }
